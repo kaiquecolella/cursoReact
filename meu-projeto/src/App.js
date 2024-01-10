373a45460bcfd53f9800ee67525f39ef7f33f411
kaiquecolella <kaique.vilaprudente@hotmail.com>
2024-01-10 10:42:58 -0300

    aula 06

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 const nome = "Maria"
 
 function App() {
   return (
     <div className='App'>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Matheus"/>
       <SayMyName nome={nome}/>
       <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150" />

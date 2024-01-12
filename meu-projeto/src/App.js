499b569af547028439a6d1de0f977b02a25ae0ad
Kaique colella <99101800+kaiquecolella@users.noreply.github.com>
2024-01-12 09:41:35 -0300

    Aula 09
    Eventos no React (onClick, onChange e onSubmit)

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
-
-const nome = "Maria"
+import Evento from './components/Evento'
+import Form from './components/Form';
 
 function App() {
   return (
     <div className='App'>
-      <Frase/>
-      <Frase/>
-      <SayMyName nome="Matheus"/>
-      <SayMyName nome={nome}/>
-      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150" />
-      <List/>
+      <h1>Testando Eventos</h1>
+      <Evento/>
+      <Form/>
     </div>
   )
 }
